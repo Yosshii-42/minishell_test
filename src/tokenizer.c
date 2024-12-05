af5b0ee7be5b6b0e178b0cb1fcedc8d709b9f7dd
Yosshii-42 <yoshi.tsurudome@gmail.com>
2024-12-06 02:47:23 +0900

    tokenizer_syntax, tokenizer_lexer fix

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tokenizer.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hurabe <hurabe@student.42.fr>              +#+  +:+       +#+        */
+/*   By: tsururukakou <tsururukakou@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 19:28:45 by hurabe            #+#    #+#             */
-/*   Updated: 2024/12/04 19:39:34 by hurabe           ###   ########.fr       */
+/*   Updated: 2024/12/06 02:36:11 by tsururukako      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,38 +52,86 @@ static	void	token_lstadd_back(t_token **head, t_token *new)
 	new->pre = ptr;
 }
 
-void	append_token(char **input, int token_len, t_token **head, t_token *new)
+void	append_quote_token(char *input, t_token **head, t_token *new, int *i)
 {
-	new->next = NULL;
-	new->word = ft_substr(*input, 0, token_len);
+	printf("here\n");
+	int		len;
+	char	quote;
+
+	len = 0;
+	quote = *input;
+	if (input[len + 1])
+		len++;
+	else
+		return ;
+	while (input[len] && input[len] != quote)
+		len++;
+	len++;
+	new->word = ft_substr(input, 0, len);
 	token_lstadd_back(head, new);
-	*input += token_len;
+	*i += len;
+}
+
+void	append_spcial_token(char *input, t_token **head, t_token *new, int *i)
+{
+	int		len;
+	char	special_char;
+
+	len = 0;
+	special_char = *input;
+	while (input[len] && input[len] == special_char)
+		len++;
+	new->word = ft_substr(input, 0, len);
+	token_lstadd_back(head, new);
+	*i += len;
+}
+
+void	append_normal_token(char *input, t_token **head, t_token *new, int *i)
+{
+	int	len;
+	
+	len = 0;
+	while (input[len] && ft_strchr(SPECIAL_CHAR, input[len]) == 0 && !ft_isspace(input[len]))
+		len++;
+	new->word = ft_substr(input, 0, len);
+	token_lstadd_back(head, new);
+	*i += len;
 }
 
 t_token	*tokenizer(char *input)
 {
 	t_token	*head;
 	t_token	*new;
-	int		token_len;
+	int		i;
 
 	head = NULL;
-	input = space_skip(input);
-	while (*input)
+	i = 0;
+	// input = space_skip(input);
+	while (input[i])
 	{
 		new = NULL;
 		new = (t_token *)safe_malloc(1, sizeof(t_token));
 		init_token(new);
-		if (ft_strchr(SPECIAL_TOKEN, *input))
-		{
-			token_len = count_meta_len(input);
-			append_token(&input, token_len, &head, new);
-		}
-		else
-		{
-			token_len = count_word_len(input, new);
-			append_token(&input, token_len, &head, new);
-		}
-		input = space_skip(input);
+		while (input[i] && ft_isspace(input[i]))
+			i++;
+		if (input[i] && (input[i] == '\'' || input[i] == '\"'))
+			append_quote_token(&input[i], &head, new, &i);
+		else if (input[i] && ft_strchr(SPECIAL_TOKEN, input[i]))
+			append_spcial_token(&input[i], &head, new, &i);
+		else if (input[i])
+			append_normal_token(&input[i], &head, new, &i);
+		i++;
+		// if (ft_strchr(SPECIAL_TOKEN, *input))
+		// {
+		// 	token_len = count_meta_len(input);
+		// 	append_token(&input, token_len, &head, new);
+		// }
+		// else
+		// {
+		// 	token_len = count_word_len(input, new);
+		// 	append_token(&input, token_len, &head, new);
+		// }
+		// input = space_skip(input);
 	}
 	return (head);
 }
