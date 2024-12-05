85b9d4b8513c826412c6403fdb7f4874ce655a27
urabexon <hiroki708.a48uk@gmail.com>
2024-12-05 21:14:12 +0900

    fix: builtin cd fix now

--- src/builtin_cd.c
+++ src/builtin_cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_cd.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tsururukakou <tsururukakou@student.42.f    +#+  +:+       +#+        */
+/*   By: hurabe <hurabe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 10:15:09 by yotsurud          #+#    #+#             */
-/*   Updated: 2024/11/26 23:03:06 by tsururukako      ###   ########.fr       */
+/*   Updated: 2024/12/05 21:10:26 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,69 +14,140 @@
 
 static void	update_env_var(t_env *env, char *key, char *value)
 {
-    t_env *current = env;
-    t_env *new_node;
+	t_env	*current;
+	t_env	*new_node;
 
-    while (current)
-    {
-        if (ft_strcmp(current->key, key) == 0)
-        {
-            free(current->value);
-            current->value = ft_strdup(value);
-            return;
-        }
-        current = current->next;
-    }
-    // 存在しない場合、新しいノードを追加
+	current = env;
+	while (current)
+	{
+		if (ft_strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = ft_strdup(value);
+			return ;
+		}
+		current = current->next;
+	}
 	new_node = (t_env *)safe_malloc(1, sizeof(t_env));
-    new_node->key = ft_strdup(key);
-    new_node->value = ft_strdup(value);
-    new_node->next = NULL;
-    lstadd_back(&env, new_node);
+	new_node->key = ft_strdup(key);
+	new_node->value = ft_strdup(value);
+	new_node->next = NULL;
+	lstadd_back(&env, new_node);
+}
+
+static char	*resolve_target_path(t_cmd *cmd)
+{
+	if (!cmd->cmd[1])
+		return (getenv_str("HOME"));
+	if (ft_strncmp(cmd->cmd[1], "-", 2) == 0)
+		return (getenv_str("OLDPWD"));
+	return (cmd->cmd[1]);
+}
+
+static void	handle_path_error(const char *cmd)
+{
+	if (cmd && ft_strncmp(cmd, "-", 2) == 0)
+		ft_printf(2, "bash: cd: OLDPWD not set\n");
+	else
+		ft_printf(2, "bash: cd: HOME not set\n");
+	end_status(SET, EXIT_FAILURE);
+}
+
+static void	handle_chdir_error(const char *path)
+{
+	ft_printf(2, "bash: cd: %s: %s\n", path, strerror(errno));
+	end_status(SET, EXIT_FAILURE);
 }
 
 void	builtin_cd(t_cmd *cmd, t_env *env)
 {
 	char	*target_path;
 	char	cwd[PATH_MAX];
+	char	*old_pwd;
 
-	// 引数が多すぎる場合エラー
-	if (cmd->cmd[2])
-	{
-		ft_printf(2, "bash: cd: too many arguments\n");
-		end_status(SET, EXIT_FAILURE);
-	}
-	// 移動パス決定
-	// 引数がない場合、HOMEへ移動する
-	if (!cmd->cmd[1])
-		target_path = getenv_str("HOME");
-	else if (ft_strncmp(cmd->cmd[1], "-", 2) == 0)
-		target_path = getenv_str("OLDPWD");
-	else
-		target_path = cmd->cmd[1];
-
+	target_path = resolve_target_path(cmd);
 	if (!target_path)
-	{
-		if (cmd->cmd[1] && ft_strncmp(cmd->cmd[1], "-", 2) == 0)
-			ft_printf(2, "bash: cd: OLDPWD not set\n");
-		else
-			ft_printf(2, "bash: cd: HOME not set\n");
-		end_status(SET, EXIT_FAILURE);
-	}
-
-	// ディレクトリ移動処理
+		handle_path_error(cmd->cmd[1]);
 	if (chdir(target_path) == -1)
-	{
-		ft_printf(2, "bash: cd: %s: %s\n", target_path, strerror(errno));
-		end_status(SET, EXIT_FAILURE);
-	}
-	// PWDとOLDPWDを更新する
+		handle_chdir_error(target_path);
 	if (!getcwd(cwd, sizeof(cwd)))
 	{
 		ft_printf(2, "bash: cd: error retrieving current directory\n");
 		end_status(SET, EXIT_FAILURE);
 	}
-    update_env_var(env, "PWD", cwd);
-	update_env_var(env, "OLDPWD", getenv_str("PWD"));
+	old_pwd = getenv_str("PWD");
+	if (cmd->cmd[2])
+	{
+		ft_printf(2, "bash: cd: too many arguments\n");
+		end_status(SET, EXIT_FAILURE);
+	}
+	if (old_pwd)
+		update_env_var(env, "OLDPWD", old_pwd);
+	update_env_var(env, "PWD", cwd);
+	//update_env_var(env, "PWD", cwd);
+	//update_env_var(env, "OLDPWD", getenv_str("PWD"));
 	end_status(SET, EXIT_SUCCESS);
 }
+
+
+//static void	update_env_var(t_env *env, char *key, char *value)
+//{
+//	t_env	*current;
+//	t_env	*new_node;
+
+//	current = env;
+//	while (current)
+//	{
+//		if (ft_strcmp(current->key, key) == 0)
+//		{
+//			free(current->value);
+//			current->value = ft_strdup(value);
+//			return ;
+//		}
+//		current = current->next;
+//	}
+//	new_node = (t_env *)safe_malloc(1, sizeof(t_env));
+//	new_node->key = ft_strdup(key);
+//	new_node->value = ft_strdup(value);
+//	new_node->next = NULL;
+//	lstadd_back(&env, new_node);
+//}
+
+//void	builtin_cd(t_cmd *cmd, t_env *env)
+//{
+//	char	*target_path;
+//	char	cwd[PATH_MAX];
+
+//	if (cmd->cmd[2])
+//	{
+//		ft_printf(2, "bash: cd: too many arguments\n");
+//		end_status(SET, EXIT_FAILURE);
+//	}
+//	if (!cmd->cmd[1])
+//		target_path = getenv_str("HOME");
+//	else if (ft_strncmp(cmd->cmd[1], "-", 2) == 0)
+//		target_path = getenv_str("OLDPWD");
+//	else
+//		target_path = cmd->cmd[1];
+//	if (!target_path)
+//	{
+//		if (cmd->cmd[1] && ft_strncmp(cmd->cmd[1], "-", 2) == 0)
+//			ft_printf(2, "bash: cd: OLDPWD not set\n");
+//		else
+//			ft_printf(2, "bash: cd: HOME not set\n");
+//		end_status(SET, EXIT_FAILURE);
+//	}
+//	if (chdir(target_path) == -1)
+//	{
+//		ft_printf(2, "bash: cd: %s: %s\n", target_path, strerror(errno));
+//		end_status(SET, EXIT_FAILURE);
+//	}
+//	if (!getcwd(cwd, sizeof(cwd)))
+//	{
+//		ft_printf(2, "bash: cd: error retrieving current directory\n");
+//		end_status(SET, EXIT_FAILURE);
+//	}
+//	update_env_var(env, "PWD", cwd);
+//	update_env_var(env, "OLDPWD", getenv_str("PWD"));
+//	end_status(SET, EXIT_SUCCESS);
+//}
